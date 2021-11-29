e58a0022e0aba191e07b837967296f2c000c5151
havak <havak@DESKTOP-H4HI7CT>
2021-11-29 13:16:52 +0300

    Proje dosyası ekle.

---
+++ Sha-256/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Sifreleme
+{
+    class Program
+    {
+        static string sifrelenmisMetin;
+        static Thread iplik;
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Şifrenizi Giriniz: ");
+            var metin = Console.ReadLine();
+            sifrelenmisMetin = SHASifreleme.SHA_512_Encrypting(metin);
+            Console.WriteLine("Şifrenizin Şifrelenmiş Metini: " + sifrelenmisMetin);
+            Console.WriteLine("\n*****************Şifremizi Kontrol Etmeye Başlıyoruz**********************\n");
+            iplik = new Thread(girisler);
+            iplik.Start();
+        }
+
+        public static void girisler()
+        {
+            while (true)
+            {
+                Thread.Sleep(10);
+                Tekrarlama();
+            }
+        }
+
+        public static void Tekrarlama()
+        {
+            Console.Write("Şifre: ");
+            var giris = Console.ReadLine();
+            var girisEncrypt = SHASifreleme.SHA_512_Encrypting(giris);
+            if (girisEncrypt == sifrelenmisMetin)
+                Console.WriteLine("Şifreyi Doğru Girdiniz");
+            else
+                Console.WriteLine("Yanlış Şifre");
+
+        }
+
+    }
+}
---
+++ Sha-256/SHASifreleme.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Sifreleme
+{
+    class SHASifreleme
+    {
+
+        public static string SHA_256_Encrypting(string deger)
+        {
+            SHA256 sha = SHA256.Create();
+            byte[] degerBytes = Encoding.UTF8.GetBytes(deger);
+            byte[] shaBytes = sha.ComputeHash(degerBytes);
+            return HashToByte(shaBytes);
+        }
+
+        public static string SHA_512_Encrypting(string deger)
+        {
+            SHA512 sha = SHA512.Create();
+            byte[] degerBytes = Encoding.UTF8.GetBytes(deger);
+            byte[] sha256byte = sha.ComputeHash(degerBytes);
+            return HashToByte(sha256byte);
+        }
+
+        public static string HashToByte(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (byte item in hash)
+            {
+                result.Append(item.ToString("x2"));
+
+            }
+
+            return result.ToString();
+        }
+
+    }
+}
